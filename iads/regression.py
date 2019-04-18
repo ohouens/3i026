f23e5be22e0783bcbbcf8a9e67e88774b0c3682c
ohouens <rohouens@yahoo.fr>
2019-04-18 20:22:24 +0200

    ajout d'un regresseur (vanillla)

---
+++ iads/regression.py
@@ -0,0 +1,61 @@
+class Regression():
+    """ Classe pour représenter un regresseur
+        Attention: cette classe est une classe abstraite, elle ne peut pas être
+        instanciée.
+    """
+
+    def __init__(self, input_dimension):
+        """ Constructeur de Regresseur
+            Argument:
+                - intput_dimension (int) : dimension d'entrée des exemples
+            Hypothèse : input_dimension > 0
+        """
+        raise NotImplementedError("Please Implement this method")
+
+    def predict(self, x):
+        """ rend la prediction sur x
+        """
+        raise NotImplementedError("Please Implement this method")
+
+    def train(self, dataset):
+        """construit le modele de regression sur la base fournit
+        """
+        raise NotImplementedError("Please implement this method")
+
+    def accuracy(self, dataset, epsilon=0.5):
+        """ Permet de calculer la qualité du système
+        """
+        s = 0
+        for i in range(dataset.size()) :
+            if self.predict(dataset.getX(i)) >=  dataset.getY(i)-epsilon and self.predict(dataset.getX(i)) < dataset.getY(i)+epsilon :
+                s += 1
+        return (s *1.0 / dataset.size()) *100
+
+class GradientBatch(Regression):
+    def __init__(self, input_dimension, learning_rate):
+        self.dimension = input_dimension
+        self.e = learning_rate
+        self.cost = 0
+        self.theta = 0
+        self.tH = []
+        self.cH = []
+
+    def cost(theta, X, y):
+        m = len(y)
+        y_hat = np.dot(X, theta)
+        cost = (1/2*m)*np.sum(np.square(y_hat-y))
+
+    def train(self, labeledSet, verbose=False):
+        indice = np.arange(labeledSet.size())
+        temoin = np.random.permutation(indice)
+        for i in temoin:
+            y_hat = np.dot(labeledSet.getX(i), self.theta)
+            self.theta = self.theta-(1/m)*self.e*np.dot(labeledSet.getX(i).T ,y_hat-labeledSet.getY(i))
+            self.tH.append(self.theta)
+            self.cH.append(self.cost(self.theta), labeledSet.getX(i), labeledSet.getY(i))
+        if(verbose):
+            print(loss)
+            print(self.theta)
+
+    def predict(self, x):
+        pass
